f6e8ebb1348e6a7c59b016d75284eacc09daccfe
sanu <sanu.sanu@ibm.com>
2022-06-28 19:21:17 +0530

    adding lambda and apigateway

---
+++ lambda/scripts/api_test.py
@@ -0,0 +1,7 @@
+import json
+
+def lambda_handler(event, context):
+    user_list = ["Sanu", "Peter"]
+    response = user_list
+    return response
+    
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  access_key = "AKIAZ6RIC2W4BFZ7MI6A"
+  secret_key = "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"
+  region = "us-east-1"
+}
